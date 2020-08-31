dce8362e82c5b913563e8f6e06b6a1ae98cbcb06
Nicole <chelseanbr@gmail.com>
2020-08-31 16:09:41 -0700

    finished 2 array problems

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,8 +4,19 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    max_profit = 0.0
+    # O(n^2) time, O(1) space - TOO SLOW!
+    # for i in range(len(prices)):
+    #     for j in range(i+1, len(prices)):
+    #         max_profit = max(max_profit, prices[j]-prices[i])
+
+    # O(n^2) time, O(1) space
+    if len(prices) >= 2:
+        min_buy = prices[0]
+        for price in prices[1:]:
+            max_profit = max(max_profit, price - min_buy)
+            min_buy = min(min_buy, price)
+    return max_profit
 
 
 if __name__ == '__main__':
