409434ae10316889095e4c709f5f2de46bc1761d
Nicole <chelseanbr@gmail.com>
2020-09-11 15:56:04 -0700

    started 10.2

--- epi_judge_python/sort_increasing_decreasing_array.py
+++ epi_judge_python/sort_increasing_decreasing_array.py
@@ -2,10 +2,37 @@ from typing import List
 
 from test_framework import generic_test
 
-
+import heapq
+import collections
 def sort_k_increasing_decreasing_array(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    def merge_sorted_arrays(sorted_arrays):
+        # In our heap, we want to store current item per array and array ID
+        array_iters = [iter(x) for x in sorted_arrays]
+
+        # Add first item per array into min heap
+        min_heap = []
+        for arr_id, it in enumerate(array_iters):
+            item = next(it, None)
+            if itemis not None:
+                heapq.heappush(min_heap, (item, arr_id))
+
+        result = []
+        while min_heap:
+            item, arr_id = heapq.heappop(min_heap)
+            result.append(item)
+            next_item = next(array_iters[arr_id], None)
+            if next_item is not None:
+                heapq.heappush(min_heap, (next_item, arr_id))
+        return result
+
+        # # Pythonic
+        # return list(heapq.merge(*sorted_arrays))
+
+    # Split A into k sorted arrays
+    
+
+    # Merge k arrays with helper
+    return merge_sorted_arrays(sorted_arrays)
 
 
 if __name__ == '__main__':
