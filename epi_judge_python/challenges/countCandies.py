05902c57ad17ae3fb3e43c6d1264fc48979beb12
Nicole <chelseanbr@gmail.com>
2020-09-22 10:59:06 -0700

    added hackerrank graph problem

---
+++ epi_judge_python/challenges/countCandies.py
@@ -0,0 +1,14 @@
+'''
+Return max product of 2 max nums from largest friend group(s)
+'''
+def countCandies(friends_nodes, friends_from, friends_to, friends_weight):
+  max_prod = 1
+  
+  return max_prod
+
+friends_nodes = 4
+friends_from = [1, 1, 2, 2, 2]
+friends_to = [2, 2, 3, 3, 4]
+friends_weight = [1, 2, 1, 3, 3]
+print(countCandies(friends_nodes, friends_from, friends_to, friends_weight))
+# -> 12
