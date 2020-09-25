816a8b7d38384bf957413ca544071804c0d8da44
Nicole <chelseanbr@gmail.com>
2020-09-25 12:02:01 -0700

    added searchPrefix function to reduce redundancies

--- epi_judge_python/practice/implement_trie.py
+++ epi_judge_python/practice/implement_trie.py
@@ -6,51 +6,47 @@ class Trie:
         """
         # Use dict
         self.trie = {}
+        self.cur = self.trie
 
     def insert(self, word: str) -> None:
         """
         Inserts a word into the trie.
         """
         # Point cur to trie
-        cur = self.trie
+        self.cur = self.trie
         # For each char, if not in trie, add dict for it in trie
         for char in word:
-            if char not in cur:
-                cur[char] = {}
-            cur = cur[char]
-        cur['*'] = True # '*' marks end of word
+            if char not in self.cur:
+                self.cur[char] = {}
+            self.cur = self.cur[char]
+        self.cur['*'] = True # '*' marks end of word
 
 
     def search(self, word: str) -> bool:
         """
         Returns if the word is in the trie.
         """
-        # Point cur to trie
-        cur = self.trie
-        # Check if each word char in trie
-        for char in (word):
-            if char not in cur:
-                return False
-            cur = cur[char]
-        # After checking all word chars, check if at end of word
-        if '*' not in cur:
+        if self.searchPrefix(word) and '*' in self.cur:
+            return True
+        else: 
             return False
-        return True
         
 
     def startsWith(self, prefix: str) -> bool:
         """
         Returns if there is any word in the trie that starts with the given prefix.
         """
-        # Point cur to trie
-        cur = self.trie
-        # Check if each prefix char in trie
-        for char in (prefix):
-            if char not in cur:
-                return False
-            cur = cur[char]
-        return True
+        return self.searchPrefix(prefix)
 
+    def searchPrefix(self, string):
+        # Point cur to trie
+        self.cur = self.trie
+        # Check if each string char in trie
+        for char in (string):
+            if char not in self.cur:
+                return False
+            self.cur = self.cur[char]
+        return True
 
 # Your Trie object will be instantiated and called as such:
 trie = Trie()
