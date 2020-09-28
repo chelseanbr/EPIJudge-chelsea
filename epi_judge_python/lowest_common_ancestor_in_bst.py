310dc68993186b653bd8108c7df0c082f8e1e67e
Nicole <chelseanbr@gmail.com>
2020-09-28 12:31:11 -0700

    LCA BST

--- epi_judge_python/lowest_common_ancestor_in_bst.py
+++ epi_judge_python/lowest_common_ancestor_in_bst.py
@@ -10,8 +10,15 @@ from test_framework.test_utils import enable_executor_hook
 
 # Input nodes are nonempty and the key at s is less than or equal to that at b.
 def find_lca(tree: BstNode, s: BstNode, b: BstNode) -> Optional[BstNode]:
-    # TODO - you fill in here.
-    return None
+# Recursive, O(n) time, O() space
+    # Continue exploring left or right if both nodes are in left (<) or right (>) substree
+    if s.data < tree.data and b.data < tree.data:
+        return find_lca(tree.left, s, b)
+    elif s.data > tree.data and b.data > tree.data:
+        return find_lca(tree.right, s, b)
+    # If nodes not both in left/right, they split, so return current node
+    else:
+        return tree
 
 
 @enable_executor_hook
