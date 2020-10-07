51dca5ac868280fd6d311944966653a7fc1cd9b1
Nicole <chelseanbr@gmail.com>
2020-10-06 17:07:43 -0700

    added checking tree symmetry

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -2,10 +2,38 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
-def is_symmetric(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
+def is_symmetric(root: BinaryTreeNode) -> bool:
+    # # Recursively
+    # def check_symmetry(left_subtree, right_subtree):
+    #     # BASE CASE
+    #     if not left_subtree and not right_subtree:
+    #         return True
+    #     elif left_subtree and right_subtree:
+    #         return (left_subtree.val == right_subtree.val and check_symmetry(left_subtree.left, right_subtree.right) \
+    #     and check_symmetry(left_subtree.right, right_subtree.left))
+    #     else:
+    #         return False
+    # return not root or check_symmetry(root.left, root.right)
+    
+    # Iteratively
+    # BFS
+    q = collections.deque([])
+    q.append(root)
+    q.append(root)
+    while q:
+        t1 = q.popleft()
+        t2 = q.popleft()
+        if not t1 and not t2:
+            continue
+        if not t1 or not t2:
+            return False
+        if t1.val != t2.val:
+            return False
+        q.append(t1.left)
+        q.append(t2.right)
+        q.append(t1.right)
+        q.append(t2.left)
     return True
-
 
 if __name__ == '__main__':
     exit(
