bd9d843ddc0231e6a07ea048d17db9fe113ba582
Nicole <chelseanbr@gmail.com>
2020-10-07 18:12:41 -0700

    redid lca

--- epi_judge_python/lowest_common_ancestor.py
+++ epi_judge_python/lowest_common_ancestor.py
@@ -7,25 +7,46 @@ from test_framework.binary_tree_utils import must_find_node, strip_parent_link
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
-
+import collections
 def lca(tree: BinaryTreeNode, node0: BinaryTreeNode,
         node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # Recursive, O(n) time, O(n) space
-    if not tree:
-        return None
-    # Return tree if node 1 or 2 
-    if node0.data == tree.data or node1.data == tree.data:
-        return tree
-    # Search left and right
-    left_search_result = lca(tree.left, node0, node1)
-    right_search_result = lca(tree.right, node0, node1)
+    # Recursive
+    Status = collections.namedtuple('Status',('num_target_nodes', 'lca'))
+    def recurse_tree(cur_node, p, q):
+        if not cur_node:
+            return Status(num_target_nodes=0, lca=None)            
+        left = recurse_tree(cur_node.left, p, q)
+        if left.num_target_nodes == 2:
+            return left
+        right = recurse_tree(cur_node.right, p, q)
+        if right.num_target_nodes == 2:
+            return right
+        num_target_nodes = (left.num_target_nodes + \
+            right.num_target_nodes + (p, q).count(cur_node))
 
-    if not left_search_result:
-        return right_search_result
-    if not right_search_result:
-        return left_search_result
-    # Return tree once both node 1 and 2 found to the left and right
-    return tree # Both left_search_result and right_search_result
+        if num_target_nodes >= 2:
+            lca = cur_node
+        else:
+            lca = None
+        return Status(num_target_nodes, lca)
+    return recurse_tree(tree, node0, node1).lca
+
+    # # Recursive, O(n) time, O(n) space
+    # if not tree:
+    #     return None
+    # # Return tree if node 1 or 2 
+    # if node0.data == tree.data or node1.data == tree.data:
+    #     return tree
+    # # Search left and right
+    # left_search_result = lca(tree.left, node0, node1)
+    # right_search_result = lca(tree.right, node0, node1)
+
+    # if not left_search_result:
+    #     return right_search_result
+    # if not right_search_result:
+    #     return left_search_result
+    # # Return tree once both node 1 and 2 found to the left and right
+    # return tree # Both left_search_result and right_search_result
 
 
 @enable_executor_hook
