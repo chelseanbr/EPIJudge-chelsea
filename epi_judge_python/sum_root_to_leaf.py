bccf89ab7ec67491853c1bdd8f2560bc9841f48b
Nicole <chelseanbr@gmail.com>
2020-10-12 17:01:36 -0700

    sum_root_to_leaf.py

--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -3,8 +3,20 @@ from test_framework import generic_test
 
 
 def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
-    # TODO - you fill in here.
-    return 0
+    # DFS
+    if not tree:
+        return 0
+    cusum = 0
+    nodes_vals = [(tree, tree.data)]
+    while nodes_vals:
+        cur_node, cur_val = nodes_vals.pop()
+        if cur_node.right:
+            nodes_vals.append((cur_node.right, cur_val * 2 + cur_node.right.data))
+        if cur_node.left:
+            nodes_vals.append((cur_node.left, cur_val * 2 + cur_node.left.data)) 
+        if not cur_node.right and not cur_node.left: # Leaf node
+            cusum += cur_val
+    return cusum
 
 
 if __name__ == '__main__':
