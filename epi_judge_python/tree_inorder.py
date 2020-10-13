fb8612dab023619c4bbc88deb7c783dfa8091433
Nicole <chelseanbr@gmail.com>
2020-10-13 15:08:04 -0700

    tree problems, recursive and iterative

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -5,8 +5,48 @@ from test_framework import generic_test
 
 
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    # # Recursive
+    # def helper(tree, result):
+    #     if not tree:
+    #         return
+    #     helper(tree.left, result)
+    #     result.append(tree.data)
+    #     helper(tree.right, result)
+    # result = []
+    # helper(tree, result)
+    # return result
+
+    # # Iterative with bools
+    # if not tree:
+    #     return []
+    # inorder = []
+    # nodes_visited = [(tree, False)] # cur_node, bool True if left visited
+    # while nodes_visited:
+    #     cur_node, visited = nodes_visited.pop()
+    #     if visited:
+    #         inorder.append(cur_node.data)
+    #     else:
+    #         if cur_node.right:
+    #             nodes_visited.append((cur_node.right, False))
+    #         nodes_visited.append((cur_node, True))
+    #         if cur_node.left:
+    #            nodes_visited.append((cur_node.left, False))
+    # return inorder
+
+    # Iterative with pointer
+    if not tree:
+        return []
+    cur = tree
+    nodes = []
+    inorder = []
+    while cur or nodes:
+        while cur:
+            nodes.append(cur)
+            cur = cur.left
+        cur = nodes.pop()
+        inorder.append(cur.data)
+        cur = cur.right
+    return inorder
 
 
 if __name__ == '__main__':
